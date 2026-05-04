ab957717702c119de8088866280699181af30735
Tom <thomas.mant@hotmail.com>
2026-05-04 13:22:03 +0100

    Complete javascript foundations exercise 11

--- foundations/11_tempConversion/tempConversion.js
+++ foundations/11_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let conversion = (temp - 32) * (5 / 9);
+  if (Number.isInteger(conversion)) return conversion;
+  return Number(conversion.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let conversion = temp * (9 / 5) + 32;
+  if (Number.isInteger(conversion)) return conversion;
+  return Number(conversion.toFixed(1));
 };
 
 // Do not edit below this line
