c83ba5ba2e27a8d7308b8d47aad52f3f1a1d6fe7
Tom <thomas.mant@hotmail.com>
2026-05-04 12:27:01 +0100

    Complete javascript foundations exercise 8

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    newArray = [];
+    
+    for (const element of array) {
+            if (!args.includes(element)) {
+                newArray.push(element);
+            };
+    };
+
+    return newArray;
 };
 
 // Do not edit below this line
