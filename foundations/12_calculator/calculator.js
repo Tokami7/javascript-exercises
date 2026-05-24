e62d0ae3af8bfc8632a4a217ce5067c210668bcc
Tom <thomas.mant@hotmail.com>
2026-05-24 11:29:23 +0100

    Complete js foundations exercise 12 calculator

--- foundations/12_calculator/calculator.js
+++ foundations/12_calculator/calculator.js
@@ -1,25 +1,31 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	const totalSum = array.reduce(((total, element) => total + element), 0);
+  return totalSum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	const totalMultiply = array.reduce(((total, element) => total * element), 1);
+  return totalMultiply;
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	let total = 1;
+  for (let i = a; i > 0; i-- ) {
+    total = total * i;
+  };
+  return total;
 };
 
 // Do not edit below this line
