d7c241692088a61176b3af417b42ada1d94ced68
Tom <thomas.mant@hotmail.com>
2026-05-25 10:35:50 +0100

    Complete js foundations exercise 13 palindromes

--- foundations/13_palindromes/palindromes.js
+++ foundations/13_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (string) {
+    const forward = string.toLowerCase().replace(/[^a-z0-9]/g, "");
+    const reverse = forward.split("").reverse().join("");
+    return forward === reverse;
 };
 
 // Do not edit below this line
