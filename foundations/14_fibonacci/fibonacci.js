3e8e8a0cb26facd988071dc550bb7a98490041c3
Tom <thomas.mant@hotmail.com>
2026-05-25 11:06:28 +0100

    Complete js foundations exercise 14 fibonacci

--- foundations/14_fibonacci/fibonacci.js
+++ foundations/14_fibonacci/fibonacci.js
@@ -1,5 +1,24 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
+    let numnum
+    if (typeof num !== 'number') {
+        numnum = parseInt(num)
+    } else {
+        numnum = num
+    }
 
+    if (numnum < 0) return "OOPS";
+    if (numnum == 0) return 0;
+
+    let number2 = 1;
+    let number1 = 0;
+
+    for (let i = 2; i <= numnum; i++) {
+        let current = number1 + number2;
+        number1 = number2;
+        number2 = current;
+    }
+
+    return number2;
 };
 
 // Do not edit below this line
