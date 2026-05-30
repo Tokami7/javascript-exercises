75d12b6aaf07bca423c5f7af3edc265d0a16aa6e
Tom <thomas.mant@hotmail.com>
2026-05-30 10:51:37 +0100

    Complete js foundations exercise 16 findTheOldest

--- foundations/16_findTheOldest/findTheOldest.js
+++ foundations/16_findTheOldest/findTheOldest.js
@@ -1,5 +1,20 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    const currentYear = (new Date()).getFullYear();
+    for (let element of people){
+        if ("yearOfDeath" in element === false) {
+            element.yearOfDeath = currentYear
+        };
+    };
 
+    let oldest = people.sort(function(a, b) {
+        const lastGuy = a.yearOfDeath - a.yearOfBirth;
+        console.log(lastGuy)
+        const nextGuy = b.yearOfDeath - b.yearOfBirth;
+        console.log(nextGuy)
+        return lastGuy > nextGuy ? -1 : 1;
+    });
+
+    return oldest[0];
 };
 
 // Do not edit below this line
